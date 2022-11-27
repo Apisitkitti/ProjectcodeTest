d0aed30add64a48c6f02797a42d306d5434d461d
apisitkitti <apisit.o@mail.kmutt.ac.th>
2022-11-27 21:39:40 +0700

    ff

--- Program.cs
+++ Program.cs
@@ -3,17 +3,26 @@
 enum MainMenu
 {
     Register = 1,
-    Login
+    Login,
+    CheckRoomm
 }
 enum RegisterMenu
 {
     Student = 1,
     Teacher
 }
+enum StudentMenu
+{
+    
+    Reserve = 1,
+    Check,
+    TLoguot
+}
 enum TeacherMenu
 {
     Accept = 1,
-    Check
+    Check,
+    TLoguot
 }
  class Program
 {
@@ -28,25 +37,34 @@ enum TeacherMenu
     static void ChooseMenu()
     {
         Console.Clear();
+        Console.WriteLine("---------------------------------------");
+        Console.WriteLine("Welcome to the room reservation system.");
+        Console.WriteLine("---------------------------------------");
         Console.WriteLine("1.Register\n2.Login\n3.Check ReserveRoom");
+        Console.WriteLine("---------------------------------------");
         ChooseMenuFromKeyboard();
     } 
     static void ChooseMenuFromKeyboard()
     {
-        Console.Write("Pls choose your Menu : ");
+       Console.Write("please choose your Menu : ");
        MainMenu menu = (MainMenu)(int.Parse(Console.ReadLine()));
        DisplayMenu(menu);
     }
     //registermenu   
      static void ChooseRegisterMenu()
     {
+        Console.Clear();
+        Console.WriteLine("--------------------------");
+        Console.WriteLine("         Register.        ");
+        Console.WriteLine("--------------------------");
         Console.WriteLine("1.Student\n2.Teacher");
+        Console.WriteLine("--------------------------");
         ChooseRegisterMenuFromKeyboard();
     }
     
      static void ChooseRegisterMenuFromKeyboard()
     {
-        Console.Write("Pls choose your Menu : ");
+        Console.Write("please choose your Menu : ");
        RegisterMenu menu = (RegisterMenu)(int.Parse(Console.ReadLine()));
        DisplayRegisterMenu(menu);
     }
@@ -58,7 +76,8 @@ enum TeacherMenu
             break;
             case MainMenu.Login: LoginFromKeyboard();
             break;
-        
+            case MainMenu.CheckRoomm: CheckReserveRoom();
+            break;
         }
     }
    
@@ -95,9 +114,9 @@ enum TeacherMenu
      }
         
     static void NewTeacher()
-    {
+     {
         Console.Clear();
-         string name = InputName();
+        string name = InputName();
         string surname = InputSurname();
         Teacher teacher = new Teacher(name,surname,InputCitizenId(),Password());
         bool checkdata = data.CheckRegis(name,surname);
@@ -120,7 +139,11 @@ enum TeacherMenu
     //Login
     static void LoginFromKeyboard()
     {
-       
+        Console.Clear();
+        Console.WriteLine("");
+        Console.WriteLine("--------------------------");
+        Console.WriteLine("           Login.         ");
+        Console.WriteLine("--------------------------");
         Console.Write("ID: ");
         string ID = Console.ReadLine();
         Console.Write("Password: ");
@@ -136,29 +159,57 @@ enum TeacherMenu
                 StudentReserve(ID,Password);
             }
         }
-        else if(data.LoginCheck(ID,Password) == false)
+         else if(data.LoginCheck(ID,Password) == false)
         {
             Console.Clear();
-            Console.WriteLine("Invalid Email/Password");
-            LoginFromKeyboard();
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("  Invalid Email/Password");
+            Console.WriteLine("    Please login again");
+            Console.WriteLine("--------------------------");
+            PressToContinue();
         }
-
+        
         //ส่วนMenuLoginนักศึกษา
         static void StudentReserve(string id , string password)
         {
             Console.Clear();
-            Console.WriteLine("Welcome");
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine("                Welcome.               ");
+            Console.WriteLine("---------------------------------------");
             data.WHoisthat(id,password);
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine("Choose Menu\n1.Reserve room\n2.Check Student\n3.Logout");
+            Console.WriteLine("---------------------------------------");
+            Console.Write("please choose your Menu : ");
+            StudentMenu menu = (StudentMenu)(int.Parse(Console.ReadLine()));
+            SwitchStudentMenu(menu);
+
         }
-        
+        static void SwitchStudentMenu(StudentMenu menu)
+        {
+            switch(menu)
+            {
+               case StudentMenu.Reserve:Reserve();
+               break; 
+               case StudentMenu.Check:Display_tudent_CheckList();
+               break;
+               case StudentMenu.TLoguot:BackToMainMemu();
+               break;
+            }
+        } 
+    
         //ส่วนMenuloginอาจารย์
         static void TeacherAccept(string id , string password)
         {
             Console.Clear();
-            Console.WriteLine("Welcome");
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine("                Welcome.               ");
+            Console.WriteLine("---------------------------------------");
             data.WHoisthat(id,password);
-            Console.WriteLine("Choose Menu\n1. Accept room\n2. Check Student");
-            Console.Write("Choose: ");
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine("Choose Menu\n1.Accept room\n2.Check Student\n3.Logout");
+            Console.WriteLine("---------------------------------------");
+            Console.Write("please choose your Menu : ");
             TeacherMenu menu = (TeacherMenu)(int.Parse(Console.ReadLine()));
             SwitchTeacherMenu(menu);
 
@@ -170,6 +221,8 @@ enum TeacherMenu
                case TeacherMenu.Accept:AdminConfirm();
                break; 
                case TeacherMenu.Check:Display_tudent_CheckList();
+               break;
+               case TeacherMenu.TLoguot:BackToMainMemu();
                break;
             }
         } 
@@ -187,37 +240,37 @@ enum TeacherMenu
     //ส่วนการจะใช้กกรอกข้อมูล
     public static string InputName()
     {
-        Console.Write("Pls input your name : ");
+        Console.Write("please input your name : ");
         return Console.ReadLine();
     }
     public static string InputSurname()
     {
-        Console.Write("Pls input your Surname : ");
+        Console.Write("please input your Surname : ");
         return Console.ReadLine();
     }
     public static string InputID()
     {
-        Console.Write("Pls input your ID : ");
+        Console.Write("please input your ID : ");
         return Console.ReadLine();
     }
     public static string InputClass_Year()
     {
-        Console.Write("Pls Input class year : ");
+        Console.Write("please Input class year : ");
         return Console.ReadLine();
     }
     public static string InputRole()
     {
-        Console.Write("Pls input your role : ");
+        Console.Write("please input your role : ");
         return Console.ReadLine();
     }
     public static string InputCitizenId()
     {
-        Console.Write("Pls input yor CitizenID : ");
+        Console.Write("please input yor CitizenID : ");
         return Console.ReadLine();
     }
     public static string Password()
     {
-      Console.Write("Pls input your Password: ");
+      Console.Write("please input your Password: ");
       return Console.ReadLine();
     }
     //เตรียมตัวเพื่อโหลดข้อมูล่วงหน้าจะได้ไม่บึ้ม
@@ -225,4 +278,39 @@ enum TeacherMenu
     {
         Program.data = new ListData();
     }   
+
+    //ส่วนเช็คห้อง
+        static void CheckReserveRoom()
+        {
+        Console.Clear();
+        Console.WriteLine("---------------------------------------");
+        Console.WriteLine("                 Room.                 ");
+        Console.WriteLine("---------------------------------------");
+        PressToContinue();
+        }
+
+    //ส่วนจอง
+        static void Reserve()
+        {
+        Console.Clear();
+        Console.WriteLine("---------------------------------------");
+        Console.WriteLine("             comming soon              ");
+        Console.WriteLine("---------------------------------------");
+        PressToContinue();
+        }
+
+    //กลับสู่ main เมนู
+        static void BackToMainMemu()
+        {
+            ChooseMenu();
+        }
+         static void PressToContinue()
+        {
+            Console.Write("Press enter to continue");
+            string enter = Console.ReadLine();
+            if (enter == "")
+            {
+                ChooseMenu();
+            }
+        }
 }
